30a9286831b38b4202a6547b7c603102c8a589bc
elkin-garzon <elkingarzonyaya@gmail.com>
2022-07-20 20:22:22 -0500

    add methods in class

--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,105 @@ export default class Query {
     /**
      * parameters search
      */
-    public params: Array<String>;
+    public params: any;
 
     //constructor
     constructor(
         table: Array<String>,
         columns: Array<String>,
-        params: Array<String>
+        params: any
     ) {
         this.table = table;
         this.columns = columns;
         this.params = params
     }
 
+    renderSQl() {
+        return {
+            select: `${this.select()} ${this.from()} ${this.where()} ${this.order()}  ${this.limit()}`,
+            count: `${this.count()}`
+        }
+    }
 
     /**
      * SELECT DE COLUMNAS DE LA TABLA
      */
     select() {
         return `SELECT ${this.columns}`;
+    }
+
+    /**
+    * Generar parte de consulta FROM SQL
+    */
+    from() {
+        let from = `FROM `;
+        if (this.table.length == 1) {
+            from = `${from} ${this.table}`
+        } else {
+            for (let i = 0; i < this.table.length; i++) {
+                if (i == 0) {
+                    from = `${from} ${this.table[i]}`
+                } else {
+                    from = `${from} INNER JOIN ${this.table[i]} ON ${this.table[0]}.id_${this.table[i]} = ${this.table[i]}.id`
+                }
+            }
+        }
+        return from;
+    }
+
+    order() {
+        if (this.params.orderby != undefined && this.params.orderColumn != undefined) {
+            return `ORDER BY ${this.params.orderColumn} ${this.params.orderby}`;
+        } else {
+            return '';
+        }
+
+    }
+
+    /**
+    * 
+    * @param paginaNumero indice de pagina a mostrar
+    * @param paginaTamano numero de registros a mostrar
+    */
+    limit() {
+        let page = Number(this.params.page) * Number(this.params.size) - Number(this.params.size);
+        return `LIMIT ${page}, ${this.params.size}`
+    }
+
+    /**
+     * condiciones que llegan en los paramtros urls
+     */
+    where() {
+        let where = `WHERE `;
+        if (this.params.filterCondition != undefined) {
+            if (Array.isArray(this.params.filterCondition)) {
+                let iteration = this.params.filterCondition.length;
+                for (let i = 0; i < iteration; i++) {
+                    where = `${where} ${this.params.filterColumn[i]} ${this.params.filterCondition[i]} ${this.params.fllterValue[i]}`;
+                    if (this.params.filterRelation) {
+                        if (this.params.filterRelation[i]) {
+                            where = `${where} ${this.params.filterRelation[i]}`;
+                        }
+                    }
+                }
+                return where;
+            } else {
+                where = `${where} ${this.params.filterColumn} ${this.params.filterCondition} ${this.params.fllterValue}`;
+            }
+            return where;
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * conteo de registros
+     */
+    count() {
+        if (this.where().length > 0) {
+            return `SELECT count(*) as total ${this.from()} ${this.where()}`;
+        } else {
+            return `SELECT count(*) as total ${this.from()}`;
+        }
     }
 }
